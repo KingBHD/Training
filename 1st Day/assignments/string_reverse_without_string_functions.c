175a4af5b44bb013b37d3af1a856af037ec1a79d
Bikram Halder <BHBHD65@gmail.com>
2021-12-14 19:44:52 +0530

    Day 1 Assignment Solution with output screenshots.

--- 1st Day/assignments/string_reverse_without_string_functions.c
+++ 1st Day/assignments/string_reverse_without_string_functions.c
@@ -2,8 +2,13 @@
 
 int main() {
     char str[] = "String";
+    char rev[6];
 
-    for (int i = 6; i >= 0; i--)
-        printf("%c", str[i]);
+    for (int i = 0; i <= 6; i++)
+        rev[i] = str[6 - i];
+
+    for (int i = 0; i <= 6; i++)
+        printf("%c", rev[i]);
+
     return 0;
 }
